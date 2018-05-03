1887b3b6a713dfd4723226a326fe83e07e4e306e
GuillermoGahbler <mexamericano@gmail.com>
2018-05-03 12:30:12 -0700

    added more tacos

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
  
 
 insert into tacos (taco_name, devoured)values("Adobada Taco",false),
-("Carnitas Taco",false),("Fish Taco",false);
+("Carnitas Taco",false),("Fish Taco",false),("Shrimp Taco",true),("Tripas Taco",true);
